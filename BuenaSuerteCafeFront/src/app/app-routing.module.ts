bca0b8e6f4230af0f821758066fcc433914c6300
Andres Felipe Campaz <dtmo96@outlook.com>
2021-08-23 17:28:33 -0500

    Login implementado

--- BuenaSuerteCafeFront/src/app/app-routing.module.ts
+++ BuenaSuerteCafeFront/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './Auth/auth.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes),
+  AuthModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
